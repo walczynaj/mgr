51d0febac738a05ceec3bf7d306991c40dd68a78
Justyna Walc <testowaniesnow@gmail.com>
2023-12-01 22:07:51 +0000

    edit

--- models/staging/dot_code_dictionary/view/BI_VW_DOT_CODE_DICTIONARY.sql
+++ models/staging/dot_code_dictionary/view/BI_VW_DOT_CODE_DICTIONARY.sql
@@ -1,10 +1,9 @@
-with DOT_CODE_DICTIONARY as (
 
-select
-    CODE ,
-    DESCRIPTION 
-
-    from AIRLINES.FLIGHTS.DOT_CODE_DICTIONARY
-)
-
-select * from DOT_CODE_DICTIONARY
+CREATE OR REPLACE VIEW AIRLINES.FLIGHTS.BI_VW_DOT_CODE_DICTIONARY  
+COMMENT = 'DOT Code dictionary'
+(
+    CODE  comment 'An identification number assigned by US DOT to identify a unique airline (carrier). A unique airline (carrier) is defined as one holding and reporting under the same DOT certificate regardless of its Code, Name, or holding company/corporation.',
+    DESCRIPTION comment 'DOT Code description')
+    
+AS SELECT *
+from AIRLINES.FLIGHTS.DOT_CODE_DICTIONARY
