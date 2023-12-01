64a97819cf58493ba3ebb1bc8dce0bc4182c66db
walczynaj <152332116+walczynaj@users.noreply.github.com>
2023-12-02 00:03:09 +0100

    Create DICTIONARY.sql

---
+++ AIRLINES/FLIGHTS/DICTIONARY.sql
@@ -0,0 +1,8 @@
+------- DICTIONARY
+
+CREATE OR REPLACE TABLE DICTIONARY (
+    Updated_Header VARCHAR(250),
+    Source_Header VARCHAR(250),
+    Data_Type VARCHAR(250),
+    Description VARCHAR(250)
+);
